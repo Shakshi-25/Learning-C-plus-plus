24adb03fc478cd9c6a074c10e7e3dde497b71eb6
Shakshi Singh <singhshakshi800@gmail.com>
2026-07-21 23:53:23 +0530

    new code

---
+++ 03_constant.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int a=20;
+    cout<<a;
+    return 0;
+}
---
+++ 04_datatype.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    float b=2.8;
+    char c='A';
+    double d=25.568;
+    bool e=true;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
