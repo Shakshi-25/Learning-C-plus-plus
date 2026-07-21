0e0d8d208304d1e5f9544b892cb195085e511ea5
Shakshi Singh <singhshakshi800@gmail.com>
2026-07-21 09:20:32 +0530

    new code
    Co-authored-by: Rajan Singh <Nimbu-paani@users.noreply.github.com>

--- grading_system.cpp
+++ grading_system.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int marks;
-    cout<<"Enter the marks obtained: ";
-    cin>>marks;
-    if(marks>=90)
+    cout << "Enter the marks obtained: ";
+    cin >> marks;
+    if (marks >= 90)
     {
-        cout<<"Grade: A++";
+        cout << "Grade: A++";
     }
-    else if(marks<90 && marks>=80)
+    else if (marks < 90 && marks >= 80)
     {
-        cout<<"Grade: A";
+        cout << "Grade: A";
     }
-    else if(marks<80 && marks>=70)
+    else if (marks < 80 && marks >= 70)
     {
-        cout<<"Grade: B";
+        cout << "Grade: B";
     }
-    else if(marks<70 && marks>=60)
+    else if (marks < 70 && marks >= 60)
     {
-        cout<<"Grade: C";
+        cout << "Grade: C";
     }
-    else if(marks<60 && marks>=50)
+    else if (marks < 60 && marks >= 50)
     {
-        cout<<"Grade: D";
+        cout << "Grade: D";
     }
-    else if(marks<50 && marks>=40)
+    else if (marks < 50 && marks >= 40)
     {
-        cout<<"Grade: E";
+        cout << "Grade: E";
     }
     else
     {
-        cout<<"Grade: Fail";
+        cout << "Grade: Fail";
     }
     return 0;
 }
